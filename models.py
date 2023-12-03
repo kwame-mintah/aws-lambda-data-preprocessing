1d2691385cb2002e33515632e27f42d467f58f1b
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-12-03 17:17:36 +0000

    fix(models): incorrect key for getting the object
    resolve keyerror in `S3Record` as the the `bucket` does not contain the object key.

--- models.py
+++ models.py
@@ -4,4 +4,4 @@ class S3Record:
     def __init__(self, event: dict):
         self.event_name = event["Records"][0]["eventName"]
         self.bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
-        self.object = event["Records"][0]["s3"]["bucket"]["key"]
+        self.object = event["Records"][0]["s3"]["object"]["key"]
