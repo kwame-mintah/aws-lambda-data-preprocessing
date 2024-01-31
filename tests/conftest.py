45bb01657798b5d6e53946c61f1f78623705133d
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-01-31 15:35:04 +0000

    test(conftest): add fixture to set bucket names
    reuseable fixture added to set s3 bucket names

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+import data_preprocessing
+
+
+@pytest.fixture(autouse=True)
+def set_bucket_name(monkeypatch):
+    monkeypatch.setattr(
+        data_preprocessing, "PREPROCESSED_INPUT_BUCKET_NAME", "unit-test"
+    )
+    monkeypatch.setattr(
+        data_preprocessing, "PREPROCESSED_OUTPUT_BUCKET_NAME", "unit-test"
+    )
